11d14f2eb3a4483c0f8f570d4415c3ffb5a5aedb
mira-ceti <ibellon@gmail.com>
2020-07-07 20:14:18 +0200

    Pipe Slice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  nombre: string = "Capitán América";
+  nombre = 'Capitán América';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
